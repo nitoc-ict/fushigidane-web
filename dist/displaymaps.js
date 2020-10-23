97deac0b8554b9be03554b6012ae553cb5982417
higashi000 <higashi.ict@gmail.com>
2020-10-23 15:48:24 +0900

    Add display route

--- dist/displaymaps.js
+++ dist/displaymaps.js
@@ -1,9 +1,40 @@
 let map;
 
-function InitMap() {
-    map = new google.maps.Map(document.getElementById('maps'), {
-        center: {
-        },
-        zoom: 19
+window.InitMap = () => {
+    const route = window.GetRouteData();
+
+
+    route.then((r) => {
+        map = new google.maps.Map(document.getElementById('maps'), {
+            center: new google.maps.LatLng(r['0'].Latitude, r['0'].Longitude),
+            zoom: 19,
+            scaleControl: true,
+        });
+
+        let request = {
+            origin: new google.maps.LatLng(r['0'].Latitude, r['0'].Longitude),
+            destination: new google.maps.LatLng(r[(r.length - 1).toString(10)].Latitude, r[(r.length-1).toString(10)].Longitude),
+            travelMode: google.maps.DirectionsTravelMode.DRIVING,
+            waypoints: [],
+        }
+
+        if (r.length > 10) {
+            r = r.slice(1, 8);
+        }
+        r.slice(1, r.length - 1).forEach(e => {
+            request.waypoints.push({location: new google.maps.LatLng(e.Latitude, e.Longitude)})
+        })
+
+        const directionsService = new google.maps.DirectionsService();
+        const directionsRenderer = new google.maps.DirectionsRenderer({
+            map: map,
+            preserveViewport: true,
+        });
+
+        directionsService.route(request, (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+                directionsRenderer.setDirections(result);
+            }
+        })
     });
 }
